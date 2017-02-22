1d4e98684df2bcf7f4652be4f86ec986ff9c8ccc
smbss <sandro.simes92@gmail.com>
2017-02-22 16:52:42 +0000

    Log in view created.

---
+++ DevChat/View/RoundedButton.swift
@@ -0,0 +1,39 @@
+//
+//  RoundedButton.swift
+//  DevChat
+//
+//  Created by smbss on 22/02/2017.
+//  Copyright © 2017 smbss All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+}
+
